4eca7387d2ee15980b394d8212a738d296b2d4be
alinakornieieva <alinakorneeva401@gmail.com>
2024-05-18 14:10:18 +0300

    Improve NaturalWonders widget

--- lib/features/natural_wonders/view/natural_wonders_screen.dart
+++ lib/features/natural_wonders/view/natural_wonders_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_layout_travel_app/uikit/ui_colors.dart';
 
 class NaturalWonders extends StatelessWidget {
   const NaturalWonders({super.key});
@@ -15,30 +16,45 @@ class NaturalWonders extends StatelessWidget {
         children: [
           Row(
             children: [
-              IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(Icons.arrow_back)),
-              const Text('Natural Wonders')
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: const Icon(Icons.arrow_back, size: 21),
+              ),
+              const SizedBox(width: 5),
+              const Text('Natural Wonders',
+                  style: TextStyle(
+                      color: UIColor.green,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600)),
             ],
           ),
+          const SizedBox(height: 36),
           Text(
               'Welcome to our travel app, your ultimate guide to discovering captivating destinations around the globe! Whether you`re seeking the tranquility visit offers something for every traveler.',
               style: theme.textTheme.bodySmall),
-          SizedBox(height: 36),
-          Text('Nature Wonders Place-1'),
-          SizedBox(height: 12),
-          Image.asset('assets/nature2.png'),
-          SizedBox(height: 12),
+          const SizedBox(height: 36),
+          Text('Nature Wonders Place-1',
+              style: TextStyle(
+                  color: UIColor.green.withOpacity(0.5),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold)),
+          const SizedBox(height: 12),
+          Center(child: Image.asset('assets/nature2.png')),
+          const SizedBox(height: 12),
           Text(
               'Welcome to our travel app, your ultimate guide to discovering captivating destinations around the globe! Whether you`re seeking the tranquility visit offers something for every traveler.',
               style: theme.textTheme.bodySmall),
-          SizedBox(height: 36),
-          Text('Nature Wonders Place-2'),
-          SizedBox(height: 12),
-          Image.asset('assets/nature1.png'),
-          SizedBox(height: 12),
+          const SizedBox(height: 36),
+          Text('Nature Wonders Place-2',
+              style: TextStyle(
+                  color: UIColor.green.withOpacity(0.5),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold)),
+          const SizedBox(height: 12),
+          Center(child: Image.asset('assets/nature1.png')),
+          const SizedBox(height: 12),
           Text(
               'Welcome to our travel app, your ultimate guide to discovering captivating destinations around the globe! Whether you`re seeking the tranquility visit offers something for every traveler.',
               style: theme.textTheme.bodySmall),
